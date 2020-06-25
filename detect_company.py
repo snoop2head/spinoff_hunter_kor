b820075878b9f3a192a5838bcf0046ec7f8a8b97
noopy <young_ahn@yonsei.ac.kr>
2020-06-25 22:38:25 +0900

    divide detection from main.py

--- detect_company.py
+++ detect_company.py
@@ -2,19 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-from get_spinoff_info import iterate_avail_articles
-from extract_company_list import yield_kospi_companies
-
-
-# spinoff_articles_list = iterate_avail_articles()
-kospi_companies = yield_kospi_companies()
-
 # for url in spinoff_articles_list:
-url = "http://www.thebell.co.kr/free/content/ArticleView.asp?key=201907040100010540000667&lcode=00&page=53&svccode=00"
-res = requests.get(url)
-soup = BeautifulSoup(res.content, "lxml")
-div = soup.find("div", {"class": "viewSection"})
-article_content = div.get_text()
-for company in kospi_companies:
-    if company in article_content:
-        print(company)
+def yield_frequency_data(url: str, companies: list) -> dict:
+
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "lxml")
+    div = soup.find("div", {"class": "viewSection"})
+    article_content = div.get_text()
+
+    # set blank values
+    frequencyData = {}
+    mentioned_companies = []
+
+    # detect companies mentioned in the article
+    for company in companies:
+        if company in article_content:
+            mentioned_companies.append(company)
+
+    # make json file for appearance frequency in single article
+    for company in mentioned_companies:
+        for word_spaced in article_content.split(" "):
+            if company in word_spaced:
+                frequencyData[company] = frequencyData.setdefault(company, 0) + 1
+    return frequencyData
+
+
+# one_data = yield_frequency_data(
+#     "http://www.thebell.co.kr/free/content/ArticleView.asp?key=201907040100010540000667&lcode=00&page=53&svccode=00"
+# )
+# print(type(one_data, kospi_companies))
+
