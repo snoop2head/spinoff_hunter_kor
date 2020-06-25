0385981b89a8bd895baa8a1cd755712e3bf6cdd8
noopy <young_ahn@yonsei.ac.kr>
2020-06-25 23:51:20 +0900

    get spinoff from one file - merge two files

--- _get_spinoff_info.py
+++ _get_spinoff_info.py
@@ -1,10 +1,28 @@
-from count_spinoffs import count_spinoffs
+from collections import Counter
+
 from extract_company_list import (
     yield_kospi_companies,
     yield_kosdaq_companies,
     yield_public_companies,
 )
 from crawl_spinoff_info import iterate_avail_articles
+from detect_company import yield_frequency_data
+
+
+def count_spinoffs(companies: list, articles: list) -> dict:
+    # sum two dictionaries as Counter Object
+    mergedFrequencyData = {}
+    sum_freq_counterObj = Counter(mergedFrequencyData)
+    for url in articles:
+        frequencyData = yield_frequency_data(url, companies)
+        freq_counterObj = Counter(frequencyData)
+        sum_freq_counterObj = sum_freq_counterObj + freq_counterObj
+        print(sum_freq_counterObj)
+
+    # tranform Counter Object into dictionary
+    sum_frequency = dict(sum_freq_counterObj)
+    print(sum_frequency)
+    return sum_frequency
 
 
 def get_spinoff_info(bundle: str) -> dict:
@@ -21,7 +39,34 @@ def get_spinoff_info(bundle: str) -> dict:
     else:
         print("choose out of kospi, kosdaq, all")
     spinoff_dictionary = count_spinoffs(companies, spinoff_articles_list)
-    print("SPINOFF DICTIONARY FORMED!")
-    print(spinoff_dictionary)
-    return spinoff_dictionary
+    sorted_spinoff_dict = {
+        k: v
+        for k, v in sorted(
+            spinoff_dictionary.items(), key=lambda item: item[1], reverse=True
+        )
+    }
 
+    print("SPINOFF DICTIONARY FORMED!")
+    print(sorted_spinoff_dict)
+    return sorted_spinoff_dict
+
+
+"""
+from crawl_spinoff_info import iterate_avail_articles
+from extract_company_list import yield_kospi_companies
+
+spinoff_articles_list = [
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006241133130960107154&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006241132074760108114&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006231009195400102978&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006220912030400103218&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006220946501400103784&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006191422511360102743&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006191355276840108314&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006171057167720103744&lcode=00&page=1&svccode=00",
+    "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006170857397900106488&lcode=00&page=1&svccode=00",
+]
+
+kospi_companies = yield_kospi_companies()
+count_spinoffs(kospi_companies, spinoff_articles_list)
+"""
