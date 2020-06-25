9f794fd6c89582055d18468b98e283cce12ef058
noopy <young_ahn@yonsei.ac.kr>
2020-06-25 20:37:50 +0900

    explicitly state return value type

--- extract_company_list.py
+++ extract_company_list.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def yield_kospi_companies():
+def yield_kospi_companies() -> list:
     # TODO: SELECT MOST RECENT FILE IN THE FOLDER
     wrangled_folder_path = "finance_data_csv_wrangled"
     file_name = "퀀트데이터2020.06.24"
@@ -10,13 +10,12 @@ def yield_kospi_companies():
     kospi_df = df.loc[df["코스피코스닥"] == "코스피"]
     kosdaq_df = df.loc[df["코스피코스닥"] == "코스닥"]
 
-    tot_public_companies = df["회사명"].to_list()
     kospi_companies = kospi_df["회사명"].to_list()
-    kosdaq_companies = kosdaq_df["회사명"].to_list()
+    print("Getting All KOSPI Companies...")
     return kospi_companies
 
 
-def yield_kosdaq_companies():
+def yield_kosdaq_companies() -> list:
     # TODO: SELECT MOST RECENT FILE IN THE FOLDER
     wrangled_folder_path = "finance_data_csv_wrangled"
     file_name = "퀀트데이터2020.06.24"
@@ -26,10 +25,11 @@ def yield_kosdaq_companies():
     kosdaq_df = df.loc[df["코스피코스닥"] == "코스닥"]
 
     kosdaq_companies = kosdaq_df["회사명"].to_list()
+    print("Getting All KOSDAQ Companies...")
     return kosdaq_companies
 
 
-def yield_public_companies():
+def yield_public_companies() -> list:
     # TODO: SELECT MOST RECENT FILE IN THE FOLDER
     wrangled_folder_path = "finance_data_csv_wrangled"
     file_name = "퀀트데이터2020.06.24"
@@ -39,4 +39,5 @@ def yield_public_companies():
     kosdaq_df = df.loc[df["코스피코스닥"] == "코스닥"]
 
     tot_public_companies = df["회사명"].to_list()
+    print("Getting All Public Companies...")
     return tot_public_companies
